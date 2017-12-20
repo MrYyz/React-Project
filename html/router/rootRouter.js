5769dfeb1abe5dc2806621feb92f48c238bdaea6
MrYyz <18825046808@139.com>
2017-12-20 21:20:38 +0800

    (ye)基本大好了轮子。替换了公共footer部分，能实现子路由跳转，点击高亮效果，修改了app.js 和 rootRouter.js 的内容(使得可以切换其他不带footer的页面，如login)，另外：修改了index.html有关rem布局的内容

--- html/router/rootRouter.js
+++ html/router/rootRouter.js
@@ -1,10 +1,48 @@
 import React from 'react'
 import {Router, Route, hashHistory} from 'react-router'
 import AppComponent from '../components/app/appComponent'
-import ProductsComponent from '../components/products/productsComponent'
 
-export default (
-    <Route path="/" component={AppComponent}>
-        <Route path="products" component={ProductsComponent}></Route>
-    </Route>
-)
+// import AppComponent from '../components/app/appComponent.js'
+import ProductsComponent from '../components/products/productsComponent.js'
+
+import ReactDOM from 'react-dom'
+// import store from '../redux/configStore.js'
+import LoginComponent from '../components/login/loginComponent.js'
+
+
+// export default (
+//     <Route path="/" component={AppComponent}>
+//         <Route path="products" component={ProductsComponent}></Route>
+//     </Route>
+// )
+
+
+export default class RootRouter extends React.Component{
+    render(){
+        return(
+            <Router history={hashHistory}>
+                <Route path="/" component={AppComponent}>
+                    <Route path="products" component={ProductsComponent}></Route>
+                </Route>
+
+                <Route path="/login" component={LoginComponent}></Route>
+            </Router>
+        )
+    }
+}
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {Provider} from 'react-redux'
+// import {Router, Route, hashHistory} from 'react-router'
+// import store from './redux/configStore'
+// import routes from './router/rootRouter'
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <Router history={hashHistory} routes={routes}/>
+//     </Provider>
+//     ,document.getElementById('app'))
