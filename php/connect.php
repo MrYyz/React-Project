ee3de906a580cd00e4b57bfae7d54e7cb203e56b
MrYyz <18825046808@139.com>
2017-12-25 17:24:22 +0800

    拷了别人的来用

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
     header("Access-Control-Allow-Origin:*");
-    $conn=new mysqli('10.3.135.52','jinqiang',"123",'itemthree');
+    $conn=new mysqli('10.3.135.52','yezhang',"123456",'itemthree');
     //检测是否连接成功
     if($conn->connect_errno){
         die('连接失败'.$conn->connect_errno);
